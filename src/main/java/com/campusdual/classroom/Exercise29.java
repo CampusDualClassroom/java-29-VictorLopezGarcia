0ff3d7279f1882f789cbb8fdeaeb4a3f352ca932
VictorLopezGarcia <victor.lopezg@udc.es>
2025-05-23 09:26:33 +0200

    Ejercicio29

--- src/main/java/com/campusdual/classroom/Exercise29.java
+++ src/main/java/com/campusdual/classroom/Exercise29.java
@@ -1,7 +1,11 @@
 package com.campusdual.classroom;
 
 public class Exercise29 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        try {
+            throw new Exception("Excepción lanzada desde main");
+        } catch (Exception e) {
+            System.out.println("Excepción capturada: " + e.getMessage());
+        }
     }
 }
